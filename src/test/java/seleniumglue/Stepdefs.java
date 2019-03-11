789bd499014a70ace1221aa610ae08d62801136f
alexnorms <alexa.normandin@mail.mcgill.ca>
2019-03-11 00:29:56 -0400

    added code to tests

--- src/test/java/seleniumglue/Stepdefs.java
+++ src/test/java/seleniumglue/Stepdefs.java
@@ -2,12 +2,15 @@ package seleniumglue;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.After;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.After;
@@ -35,23 +38,147 @@ public class Stepdefs {
         driver.navigate().to("https://mail.google.com/");
     }
 
+    @Before
+    public void initialState(){
+        //set the email
+        System.out.println("Attempting to enter email to login.");
+        WebElement email = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/content[1]/section[1]/div[1]/content[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]")));
+        email.sendKeys("alexyunus428@gmail.com");
 
+        //click on next
+        WebElement next = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/content[1]/span[1]")));
+        next.click();
+        System.out.println("Email entered");
 
-    @Given("^Open the Firefox and launch the application$")
-    public void open_the_Firefox_and_launch_the_application() throws Throwable
-    {
-        System.out.println("This Step open the Firefox and launch the application.");
+        //set the password
+        System.out.println("Attempting to enter password to login.");
+        WebElement password = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/content[1]/section[1]/div[1]/content[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]")));
+        password.sendKeys("password428");
+
+        //click on next
+        WebElement btn = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/content[1]/span[1]")));
+        btn.click();
+        System.out.println("Password entered");
     }
 
-    @When("^Enter the Username and Password$")
-    public void enter_the_Username_and_Password() throws Throwable
-    {
-        System.out.println("This step enter the Username and Password on the login page.");
+
+    @After
+    public void resetInitialState(){
+        // clicking on google account button first to collapse what’s inside
+        System.out.println("Attempting to return to inbox page.");
+        WebElement inbox = (new WebDriverWait(driver, 20))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[7]/div[3]/div[1]/div[1]/div[4]/header[1]/div[2]/div[1]/div[4]/div[1]/a[1]/img[1]")));
+        inbox.click();
+        System.out.println("Back to inbox page.");
+        driver.quit();
     }
 
-    @Then("^Reset the credential$")
-    public void Reset_the_credential() throws Throwable
-    {
-        System.out.println("This step click on the Reset button.");
+    @Given("^I am logged into a Gmail account$")
+    public void i_am_logged_into_a_Gmail_account(){
+        System.out.println("I am logged in");
+    }
+
+    @When("^I click on New message$")
+    public void i_click_on_New_message(){
+        //click on new message
+        System.out.println("Attempting to find the new message button.. ");
+        WebElement btn = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[7]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
+        System.out.print("Found!\n");
+        btn.click();
+
+    }
+
+    @And("^I enter \"([^\"]*)\" as recipient address$")
+    public void i_enter_a_desired_recipient_address(String string) throws InterruptedException {
+        //set email recipient
+        System.out.println("Attempting to enter email recipient.");
+        WebElement btn = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.name("to")));
+        btn.sendKeys(string + Keys.ENTER);
+        System.out.println("Email recipient set");
+    }
+
+    @And("^I enter \"([^\"]*)\" as recipient CC$")
+    public void i_enter_a_desired_recipientCC_address(String string) throws InterruptedException {
+        //set email recipient
+        System.out.println("Attempting to enter CC email recipient.");
+        WebElement btn = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[aria-label='Add Cc recipients \u202A(⌘⇧C)\u202C']")));
+        btn.click();
+
+        WebElement cc = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.name("cc")));
+        cc.click();
+        cc.sendKeys(string + Keys.ENTER);
+        System.out.println("CC Email recipient set");
+    }
+
+    @And("^I enter \"([^\"]*)\" as invalid recipient address$")
+    public void i_enter_an_invalid_recipient_address(String string) throws InterruptedException {
+        //set invalid email recipient
+        System.out.println("Attempting to enter invalid email recipient.");
+        WebElement btn = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.name("to")));
+        btn.sendKeys(string + Keys.ENTER);
+        System.out.println("Email invalid recipient set");
+    }
+
+
+    @And("^I enter \"([^\"]*)\" as the file in body text$")
+    public void i_enter_a_desired_in_body_text(String string) throws InterruptedException {
+        //set email body text
+        System.out.println("Attempting to set body text.");
+        WebElement text = (new WebDriverWait(driver, 20))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[26]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[4]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/div[2]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/div[2]/div[1]")));
+        text.click();
+        text.sendKeys("Hi! I have inserted an image attachment!");
+        System.out.println("Body text set");
+
+        // find the input element
+        WebElement elem = driver.findElement(By.xpath("//input[@type='file']"));
+        elem.sendKeys("/Users/antoine/Documents/Pictures/" + string);
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.dO")));
+    }
+
+
+    @And("^I click on send$")
+    public void i_click_on_send() throws InterruptedException {
+        //click on send email
+        System.out.println("Attempting to send the email.");
+        WebElement send = (new WebDriverWait(driver, 20))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[26]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[4]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/div[1]/div[4]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[2]")));
+        send.click();
+        WebElement invalid = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.name("ok")));
+        invalid.click();
+        WebElement garbage = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[26]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/img[3]")));
+        garbage.click();
+
+        (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfElementLocated(By.className("aT")));
+    }
+
+    @Then("^I have sent an email with an attachment to the proper recipient$")
+    public void i_have_sent_an_email_with_an_attachment_to_the_proper_recipient() throws InterruptedException {
+        WebElement emailStatus = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.className("aT")));
+        try {
+            while(emailStatus.getText().contains("Sending"));
+        } catch (Exception e){
+            emailStatus = (new WebDriverWait(driver, 10))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.className("aT")));
+        }
+        Assert.assertTrue(confirmEmailSent(emailStatus));
+        System.out.println("Confirmation that email has been sent.");
+        (new WebDriverWait(driver, 5)).until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.dO")));
+    }
+
+    private boolean confirmEmailSent(WebElement messageSentPopup){
+        return messageSentPopup.getText().contains("Message sent");
     }
 }
--- target/test-classes/assignmentB/EmailTest.feature
+++ target/test-classes/assignmentB/EmailTest.feature
@@ -1,13 +1,63 @@
-Feature: Reset functionality on login page of Application
+Feature: Send email with an image attachment
+
+  #Normal Scenario
+  Scenario Outline: Send email with an image attachment
+
+    Given I am logged into a Gmail account
+    When I click on New message
+    And I enter "<email>" as recipient address
+    And I enter "<attachment>" as the file in body text
+    And I click on send
+    Then I have sent an email with an attachment to the proper recipient
+
+    Examples:
+      | email                         | attachment |
+      | alexanormandin@hotmail.com    | 1.jpg      |
+      | alexnorms6528@gmail.com       | 2.jpg      |
+      | alexa.normandin@mail.mcgil.ca | 3.jpg      |
+      |                  | 4.jpg      |
+      |                  | 5.jpg      |
 
 
-  Scenario: Verification of Reset button
+
+    #Alternate FLow
+  Scenario Outline: An authenticated user logs in to send an email with
+   attached image to 2 different recipients then logs out
+
+     Given I am logged into a Gmail account
+     When I click on New message
+     And I enter "<email>" as recipient address
+     And I enter "<ccemail>" as recipient CC
+     And I enter "<attachment>" as the file in body text
+     And I click on send
+     Then I have sent an email with an attachment to the proper recipient
 
 
-    Given Open the Firefox and launch the application
+     Examples:
+       | email                    |               ccemail           | attachment |
+       | jochampion17@gmail.com   |     jingxujaster@hotmail.com    |  1.jpg     |
+       | jingxujaster@hotmail.com |        jochampion17@gmail.com   |  2.jpg     |
+       | jackjingxu@yahoo.com     |        jingxujaster@hotmail.com | 3.jpg      |
+       | abc@gmail.com            |      jingxujaster@hotmail.com   |  4.jpg     |
+       | bbc@gmail.com            |       jingxujaster@hotmail.com  | 5.jpg      |
 
 
-    When Enter the Username and Password
 
+     #Error Flow
+   Scenario Outline: An authenticated user logs in to send an email with
+   attached image to 2 different recipients then logs out
 
-    Then Reset the credential
+     Given I am logged into a Gmail account
+     When I click on New message
+     And I enter "<email>" as invalid recipient address
+     And I enter "<attachment>" as the file in body text
+     And I click on send
+     Then I have sent an email with an attachment to the proper recipient
+
+     Examples:
+       | email                   | attachment |
+       | jlkds                   | 1.jpg      |
+       | jingter sdsf            | 2.jpg      |
+       | jakjixu                 | 3.jpg      |
+       | bc                      | 4.jpg      |
+       | bb@g                    | 5.jpg      |
